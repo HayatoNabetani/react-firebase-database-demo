4a229f5a5be45a4864ab2cc295f9c8794d320dc7
HayatoNabetani <nabe_baseball1111@yahoo.co.jp>
2022-03-22 18:26:38 +0900

    firebaseから情報取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
-import './App.css';
+import "./App.css";
+import db from "./firebase";
+import { useState, useEffect } from "react";
+import { collection, getDocs, doc, onSnapshot } from "firebase/firestore";
 
 const App = () => {
+  const [posts, setPosts] = useState<any>([]);
+  useEffect(() => {
+    // データベースから情報取得
+    const postData = collection(db, "posts");
+    console.log(postData);
+    getDocs(postData).then((snapShot) => {
+      setPosts(snapShot.docs.map((doc) => ({ ...doc.data() })));
+    });
+
+    // リアルタイム
+    onSnapshot(postData, (post) => {
+      setPosts(post.docs.map((doc) => ({ ...doc.data() })));
+    });
+  }, []);
+
   return (
     <div className="App">
-      App
+      <div>
+        {posts.map((post: any) => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.text}</p>
+            {post.imgUrl != "" && <img src={post.imgUrl} className="img"/>}
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyANlT_YohIHm6zxnVDRm92Vw21FxyA6i94",
+    authDomain: "fir-react-tutorial-ecca6.firebaseapp.com",
+    projectId: "fir-react-tutorial-ecca6",
+    storageBucket: "fir-react-tutorial-ecca6.appspot.com",
+    messagingSenderId: "6165649891",
+    appId: "1:6165649891:web:f8c42457febfb2fbe3e8d6",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default db;
