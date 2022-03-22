c91af79a332f17251c57ff5a6b321c7c5b628b12
HayatoNabetani <nabe_baseball1111@yahoo.co.jp>
2022-03-22 23:30:44 +0900

    アップロードしたものを表示するまで

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import "./App.css";
 import { db, storage } from "./firebase";
 import { useState, useEffect } from "react";
-import { collection, getDocs, doc, onSnapshot } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  doc,
+  onSnapshot,
+  addDoc,
+  serverTimestamp,
+} from "firebase/firestore";
 import {
   getStorage,
   ref,
@@ -26,93 +33,81 @@ const App = () => {
     });
   }, []);
 
-  const uploadHandler = (type:string,file:any): string => {
+  const uploadHandler = (type: string, file: any): any => {
     const name = file.name;
     const storage = getStorage();
     const storageRef = ref(storage, `${type}/${name}`);
     const metadata = {
       contentType: file.type,
     };
-    const uploadTask = uploadBytesResumable(storageRef, file, metadata);
-    uploadTask.on(
-      "state_changed",
-      (snapshot: any) => {
-        const progress =
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log("Upload is " + progress + "% done");
-        switch (snapshot.state) {
-          case "paused":
-            console.log("Upload is paused");
-            break;
-          case "running":
-            console.log("Upload is running");
-            break;
-        }
-      },
-      (error: any) => {
-        // Handle unsuccessful uploads
-        switch (error.code) {
-          case "storage/unauthorized":
-            // User doesn't have permission to access the object
-            break;
-          case "storage/canceled":
-            // User canceled the upload
-            break;
-
-          // ...
-
-          case "storage/unknown":
-            // Unknown error occurred, inspect error.serverResponse
-            break;
-        }
-        return "fail";
-      },
-      () => {
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-          console.log("File available at", downloadURL);
-          return downloadURL;
-        });
-      }
-    );
-    return "fail";//これなぜか必要になっちゃう
-  }
-
-  const setImageFile = (imgFile: any): string => {
-    // アップロード処理
-    console.log("画像アップロード処理");
-    const file = imgFile;
-    const type: string = "image";
-    const path:string = uploadHandler(type, file);
-    return path;
+    const uploadTask = uploadBytes(storageRef, file, metadata);
+    return uploadTask;
   };
 
+  const setImageFile = async (imgFile: any) => {
+    try {
+      console.log("画像アップロード処理");
+      const file = imgFile;
+      const type: string = "images";
+      const path: any = uploadHandler(type, file);
+      return path;
+    } catch (e) {
+      return "reject";
+    }
+  };
 
-  const setVideoFile = (videoFile:any) => {
-    // アップロード処理
-    console.log("ビデオアップロード処理");
-    const file = videoFile;
-    const type: string = "videos";
-    const path: string = uploadHandler(type, file);
-    return path;
-  }
+  const setVideoFile = async (videoFile: any) => {
+    try {
+      console.log("動画アップロード処理");
+      const file = videoFile;
+      const type: string = "videos";
+      const path: any = uploadHandler(type, file);
+      return path;
+    } catch (e) {
+      return "reject";
+    }
+  };
 
-
-  const handleSubmit = async (event:any) => {
+  const handleSubmit = async (event: any) => {
     event.preventDefault();
+    // ファイルがあれば、アップロードする処理
     const { title, text, imgFile, videoFile } = event.target.elements;
-    console.log(title.value);
-    console.log(text.value);
-    console.log(imgFile.files[0]);
-    console.log(videoFile.files[0]);
-    
     let imgFilePath = "";
     let videoFilePath = "";
     if (imgFile.files[0] !== undefined) {
-      imgFilePath = setImageFile(imgFile.files[0]);
-    };
+      await setImageFile(imgFile.files[0])
+        .then((result: any) => {
+          getDownloadURL(result.ref).then((downloadURL) => {
+            console.log("File available at", downloadURL);
+            imgFilePath = downloadURL;
+            // DBに登録する処理
+            addDoc(collection(db, "posts"), {
+              title: title.value,
+              text: text.value,
+              timestamp: serverTimestamp(),
+              imgUrl: imgFilePath,
+              videoUrl: videoFilePath,
+            });
+          });
+        });
+    }
     if (videoFile.files[0] !== undefined) {
-      videoFilePath = setVideoFile(videoFile.files[0]);
-    };
+      await setVideoFile(videoFile.files[0])
+        .then((result: any) => {
+          getDownloadURL(result.ref).then((downloadURL) => {
+            console.log("File available at", downloadURL);
+            videoFilePath = downloadURL;
+            // DBに登録する処理
+            addDoc(collection(db, "posts"), {
+              title: title.value,
+              text: text.value,
+              timestamp: serverTimestamp(),
+              imgUrl: imgFilePath,
+              videoUrl: videoFilePath,
+            });
+          });
+        });
+    }
   };
 
   return (
@@ -133,7 +128,7 @@ const App = () => {
           </div>
           <div>
             <label>videoFile</label>
-            <input name="videoFile" type="file"/>
+            <input name="videoFile" type="file" />
           </div>
           <div>
             <button>登録</button>
@@ -142,10 +137,20 @@ const App = () => {
       </div>
       <div>
         {posts.map((post: any) => (
-          <div key={post.id}>
+          <div key={post.title}>
             <h1>{post.title}</h1>
             <p>{post.text}</p>
-            {post.imgUrl != "" && <img src={post.imgUrl} className="img" />}
+            {post.imgUrl !== "" ? (
+              <img src={post.imgUrl} className="img" />
+            ) : (
+              <br />
+            )}
+
+            {post.videoUrl !== "" ? (
+              <video controls width="250" src={post.videoUrl} />
+            ) : (
+              <br />
+            )}
           </div>
         ))}
       </div>
